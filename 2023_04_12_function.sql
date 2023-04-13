0c1c9250b66b72fb5f00c5f93bdba1b26df8e764
JJSbok <sprpsa33@naver.com>
2023-04-13 11:59:54 +0900

    0412

--- 2023_04_12_function.sql
+++ 2023_04_12_function.sql
@@ -1,3 +1,26 @@
 select *
 from emp
 where hiredate = str_to_date('02198120','%m%Y%d')
+
+select*
+from emp
+
+select ename, deptno,
+case when deptno = 10 then 'accounting'
+	 when deptno = 20 then 'research'
+	 when deptno = 30 then 'sales'
+	 when deptno = 40 then 'operations'
+	end as dname
+	from emp;
+	
+SELECT sum(sal)
+from emp;
+
+select sum(comm)
+from emp;
+select comm from emp 
+where comm is not null;
+
+select avg(sal), truncate(avg(sal), 0), round(avg(sal),2)
+from emp;
+
