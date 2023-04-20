b966a668d6d72912c5d0993669318b9f792bcce0
JJSbok <sprpsa33@naver.com>
2023-04-20 13:11:01 +0900

    0420

--- 2023_04_12_function.sql
+++ 2023_04_12_function.sql
@@ -2,6 +2,8 @@ select *
 from emp
 where hiredate = str_to_date('02198120','%m%Y%d')
 
+select deptno, dname, loc from dept order by deptno;
+
 select*
 from emp
 
@@ -18,9 +20,60 @@ from emp;
 
 select sum(comm)
 from emp;
+
 select comm from emp 
 where comm is not null;
 
 select avg(sal), truncate(avg(sal), 0), round(avg(sal),2)
 from emp;
+
+
+--최대 급여 max, 최소 급여 min() 
+
+SELECT max(sal), min(sal)
+from emp;
+
+
+SELECT *
+from emp
+order by deptno 
+
+-- group by > 행 단위로 그룹핑
+
+select deptno , 
+count(*) as `사원의 수`, 
+sum(sal) as `총 급여액`, 
+max(sal) as `최고 급여`,
+min(sal) as `최소 급여액`
+from emp 
+group by deptno;
+
+
+SELECT deptno, avg(sal)
+from emp
+group by deptno
+having sum(sal) > 10000
+
+
+
+select *
+from emp
+where substr(hiredate,4,2) = '04';
+
+select *
+from emp e where mod(empno, 2) = 0;
+
+SELECT  hiredate, date_format(hiredate, 'yy/%m/%d dy')
+from emp
+
+select date(sysdate()-str_to_date('2018/01/01','%Y/%m/%d')) days
+from dual; 
+
+select empno, ename, nvl2(mgr, mgr, 0)
+from emp;
+
+SELECT *
+from emp
+
+
 
